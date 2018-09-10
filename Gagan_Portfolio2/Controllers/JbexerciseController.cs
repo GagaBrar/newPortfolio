bd410b45e8fe38a6c166fcdfa91ae1cc18ecd24f
Gagan Brar <brargagandeepsingh87@gmail.com>
2018-09-10 09:36:17 -0500

    first commit

---
+++ Gagan_Portfolio2/Controllers/JbexerciseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gagan_Portfolio2.Controllers
+{
+    public class JbexerciseController : Controller
+    {
+        // GET: Jbexercise
+        public ActionResult jbexercise()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gagan_Portfolio2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gagan_Portfolio2.MvcApplication" Language="C#" %>
